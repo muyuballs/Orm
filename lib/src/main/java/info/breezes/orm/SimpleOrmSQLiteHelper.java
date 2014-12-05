e151fe22162cdf610e5a36448e9018f381cdc107
Qiao <hello@fore-time.com>
2014-12-05 21:05:39 +0800

    Optimize speed

--- lib/src/main/java/info/breezes/orm/SimpleOrmSQLiteHelper.java
+++ lib/src/main/java/info/breezes/orm/SimpleOrmSQLiteHelper.java
@@ -3,13 +3,22 @@ package info.breezes.orm;
 import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import info.breezes.orm.utils.TableUtils;
 
 /**
- * Created by jianxingqiao on 14-6-15.
+ * Created by Qiao on 14-6-15.
  */
-public abstract class SimpleOrmSQLiteHelper extends OrmSQLiteHelper {
+public class SimpleOrmSQLiteHelper extends OrmSQLiteHelper {
 
+    public static final String TAG = "SimpleOrmSQLiteHelper";
     private SQLiteDatabase database;
+    private Class<?>[] tables;
+
+    public SimpleOrmSQLiteHelper(Context context, String name, int version, Class<?>... tables) {
+        this(context, name, null, version);
+        this.tables = tables;
+    }
 
     public SimpleOrmSQLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         this(context, name, factory, version, null);
@@ -19,13 +28,50 @@ public abstract class SimpleOrmSQLiteHelper extends OrmSQLiteHelper {
         super(context, name, factory, version, errorHandler);
     }
 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        if(tables!=null && tables.length>0) {
+            for (Class<?> table: tables) {
+                if(table!=null) {
+                    TableUtils.createTable(db, table);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 
     @Override
     public SQLiteDatabase getCurrentDatabase(boolean writable) {
         if (database == null) {
             database = writable ? getWritableDatabase() : getReadableDatabase();
+        } else if (database.isReadOnly() && writable) {
+            database.close();
+            database = getWritableDatabase();
         }
         return database;
     }
 
+    public SQLiteDatabase beginTransaction() {
+        getCurrentDatabase(true);
+        if (!database.inTransaction()) {
+            database.beginTransaction();
+        } else {
+            Log.w(TAG, "current database already in transaction.");
+        }
+        return database;
+    }
+
+    public void commit() {
+        database.setTransactionSuccessful();
+        database.endTransaction();
+    }
+
+    public void rollback() {
+        database.endTransaction();
+        ;
+    }
 }
